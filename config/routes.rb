c3afc3d31b06bb7c21875f1458c6bfd5321ea109
Khanh.L.T <ltk.hust@gmail.com>
2018-10-08 02:03:27 +0700

    Revert "fix more bug :weary: :weary: :weary: :weary: :weary: :weary::weary: :weary: :weary: :weary:"

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root 'game#index'
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
