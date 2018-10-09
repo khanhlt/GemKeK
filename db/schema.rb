671acdd15a9b9fcfca6f88d4757d6800f1a3aae6
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2018-10-09 18:17:27 +0000

    all email relative work done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_06_141943) do
+ActiveRecord::Schema.define(version: 2018_10_09_150018) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content", null: false
@@ -89,6 +89,10 @@ ActiveRecord::Schema.define(version: 2018_10_06_141943) do
     t.datetime "updated_at", null: false
     t.string "full_name"
     t.boolean "is_admin"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
