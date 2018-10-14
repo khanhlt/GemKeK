ff70bbaf178b088eb61cc0d0d4455ab870bade13
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2018-10-14 18:00:32 +0000

    write review

--- db/migrate/20181004163206_create_reviews.rb
+++ db/migrate/20181004163206_create_reviews.rb
@@ -1,7 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-      t.string :content, null: false
+      t.string :content, null: true
+      t.string :title, :default => "untitled"
       t.integer :rating
       t.integer :user_id, null: false
       t.integer :game_id, null: false
