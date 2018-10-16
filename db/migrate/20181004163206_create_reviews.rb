5b9ce38a2e7a65b43d8f9ece3d76d8155e4240f9
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2018-10-16 15:27:49 +0000

    clear review form

--- db/migrate/20181004163206_create_reviews.rb
+++ db/migrate/20181004163206_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.string :content, null: true
-      t.string :title, :default => "untitled"
+      t.string :title, null: true
       t.integer :rating
       t.integer :user_id, null: false
       t.integer :game_id, null: false
