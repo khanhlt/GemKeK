32f4d2c45237994944e9f70f37ab91c3fb4cd9b6
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2018-10-16 17:44:14 +0000

    rating

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   root 'game#index'
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :reviews,          only: [:create, :destroy, :edit, :new, :update]
+  resources :reviews,          only: [:create, :destroy, :edit, :new, :update, :rate]
 end
