5b818620daeac40a706bfd666024dc90a2b9e8f3
Thang Truong <thangtink23@gmail.com>
2018-10-22 13:57:27 +0700

    add feature profile

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,4 +1,9 @@
 class UserProfileController < ApplicationController
   def home
+  	@user = current_user
+  	@reviews = @user.reviews.includes(comments: :user)
+  	#binding.pry
+	@reviews_like = Review.where(id: @user.likes.where(likeable_type: Review.name))
+	@reviews_comment = @user.comments.map{|c| c.review}.uniq
   end
 end
