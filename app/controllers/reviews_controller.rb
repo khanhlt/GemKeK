a7692cfef0a85eb0a9c7225f57eaf20a23986034
khanhlt <ltk.hust@gmail.com>
2018-10-22 17:28:57 +0700

    add comment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,17 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :destroy, :edit]
-  before_action :find_review, only: [ :update, :destroy]
+  before_action :find_review, only: [:update, :destroy]
   before_action :correct_user, only: [:update, :destroy]
   skip_before_action :verify_authenticity_token
 
 
   def index
   end
-  
+
 
   def create
-      @review = current_user.reviews.new review_params
-      @review.game_id = params[:game_id]
+    @review = current_user.reviews.new review_params
+    @review.game_id = params[:game_id]
     if review.save
       flash[:success] = "review created!"
       redirect_to game_detail_path(id: @review.game_id)
@@ -24,60 +24,60 @@ class ReviewsController < ApplicationController
   def update
     if @review.update_attributes review_params
       flash[:notice] = "Successfully updated post!"
-      redirect_to  request.referrer
+      redirect_to request.referrer
     else
       flash[:alert] = "Error updating post!"
       render :edit
     end
   end
-  
+
   def show
     @review = Review.find(params[:id])
     @game = Game.find(@review.game_id)
   end
-  
+
   def destroy
     if review.destroy
       flash[:notice] = "Successfully deleted review!"
-      redirect_to  request.referrer
+      redirect_to request.referrer
     else
       flash[:alert] = "Error updating post!"
     end
   end
 
   def rate
-    @reviews = current_user.reviews 
-    if(@reviews.where(game_id: params[:game_id]).count >0)
+    @reviews = current_user.reviews
+    if (@reviews.where(game_id: params[:game_id]).count > 0)
       review = @reviews.where(game_id: params[:game_id]).first
     else
       review = current_user.reviews.new review_params
       review.game_id = params[:game_id]
-    end  
+    end
     review.rating = params.require(:review)[:rating]
-      
+
     if review.save
       flash[:success] = "review created!"
     else
       flash[:danger] = "Fail"
     end
   end
-  
+
   private
-  
+
   attr_reader :review
 
   def review_params
     params.require(:review).permit :content, :title, :rating
   end
-  
+
   def find_review
     @review = Review.find(params[:id])
   end
-  
+
   def correct_user
-      @review = current_user.reviews.find_by(id: params[:id])
-      redirect_to root_url if @review.nil?
+    @review = current_user.reviews.find_by(id: params[:id])
+    redirect_to root_url if @review.nil?
   end
-  
-    
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :reviews,          only: [:create, :destroy, :rate, :show, :update] do
     post 'reviews_rate' , to: 'reviews#rate'
+    resources :comments
   end
   #get 'reviews_show', to: 'reviews#show'
    resources :likes, only: [:create, :destroy]
