cc4fe02e3b21aac1df5e72f891f25b5e9d46c61a
ThanhChinhBK <nguyenthanhchinh96@gmail.com>
2018-10-31 15:39:29 +0000

    delete some annoying

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   get 'user_profile/home'
-  get 'login/home'
   get 'game/index'
   get 'game/detail'
   get 'game/game_list_of_platform'
   get 'game/game_list_of_genre'
-  get 'gameshousai/home'
   get 'search/index'
   get 'search/advance_search'
   root 'game#index'
@@ -17,7 +15,6 @@ Rails.application.routes.draw do
     resources :comments
   end
    post 'reviews_rate' , to: 'reviews#rate'
-  #get 'reviews_show', to: 'reviews#show'
    resources :likes, only: [:create, :destroy]
 
   namespace :manage do
