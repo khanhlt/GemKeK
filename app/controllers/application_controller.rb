15f7c2b866e8143fe410c6766fb1a3803e98a83c
khanhlt <ltk.hust@gmail.com>
2018-11-13 23:47:08 +0700

    fix error when sign out at profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,5 +29,13 @@ class ApplicationController < ActionController::Base
       root_path
     end
   end
-   
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+      new_user_session_path
+    else
+      root_path
+    end
+  end
 end
