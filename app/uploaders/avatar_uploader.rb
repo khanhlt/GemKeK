e82ef3d548342d0f074395fcefbfbaa7a7f6f558
khanhlt <ltk.hust@gmail.com>
2018-12-04 22:21:41 +0700

    check file upload in sigup/edit profile

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -12,6 +12,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
   
   def default_url
        'avatar.png'
